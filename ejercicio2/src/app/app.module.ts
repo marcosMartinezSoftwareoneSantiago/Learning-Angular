2ad849481ad014d93d73a090fccffcf87aa60ac6
marcosMartinezSoftwareoneSantiago <Marcos.Martinez@Softwareone.com>
2022-11-15 15:23:49 +0100

    I need to add ngmodule at homeComponent.html

--- ejercicio2/src/app/app.module.ts
+++ ejercicio2/src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { EmailComponent } from './components/email/email.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    EmailComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
